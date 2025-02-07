fabaa9ff717a4dec2aa5040a09b4db736ac29a2c
Fabio Salvagno <fabiosalvagno@yahoo.co.jp>
2025-02-07 17:59:10 +0900

    added viewport

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,6 +6,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Productivity Tips</title>
   <!-- Load Bootstrap CSS from CDN -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
